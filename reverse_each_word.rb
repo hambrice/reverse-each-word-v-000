4a2548fe311f740d599fc2e56c7efca0b17475d2
Brad Hambrice <hambrice@uab.edu>
2018-01-05 04:39:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@
 def reverse_each_word(string)
   new_array = []
   array = string.split(" ")
-  array.collect do |word|
-   new_array.push(word.reverse)
+#  array.collect do |word|
+#   new_array.push(word.reverse)
+    array.collect {|word| word.reverse}.join(" ")
   end
-  new_array.join(" ")
   
 end
